f80a98a6f5f9ea9904d1d346dc1b3696db1c7f73
ZhluktenkoDmytro <d1mnewz@gmail.com>
2016-11-01 15:12:20 +0200

    adding statistics
    reworked my sample a bit so now it uses delegate
    gives some good looking results

--- StringReverse/Program.cs
+++ StringReverse/Program.cs
@@ -9,56 +9,101 @@ namespace StringReverse
 {
     class Program
     {
-        public static void ReverseWithXOR(ref char[] arr)
+        delegate char[] CharArrDelegate(char[] s);
+
+        static void Benchmark(string description, CharArrDelegate d, int times, char[] text) // getting timings
         {
-            int start = 0;
-            int end = arr.Count() -1;
-            while (start < end)
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int j = 0; j < times; j++)
             {
-                arr[start] ^= arr[end];
-                arr[end] ^= arr[start];
-                arr[start] ^= arr[end];
-                ++start;
-                --end;
+                d(text);
             }
-           
+            sw.Stop();
+            Console.WriteLine("{0}  {1} : called {2} times.", sw.Elapsed, description, times);
         }
-        public static void ReverseArray(ref char[] arr)
+        public static char[] StringOfLength(int length) // function helper to create random string with {length} symbols
+        {
+            Random random = new Random();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))));// just get random char
+            }
+            return sb.ToString().ToCharArray();
+        }
+        public static char[] ReverseXOR(char[] str)
+        {
+            
+            int length = str.Length - 1;
+
+            for (int i = 0; i < length; i++, length--)
+            {
+                str[i] ^= str[length];
+                str[length] ^= str[i];
+                str[i] ^= str[length];
+            }
+
+            return str;
+        }
+
+        public static char [] ReverseSB(char[] text)
+        {
+            StringBuilder builder = new StringBuilder(text.Length);
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                builder.Append(text[i]);
+            }
+            text = builder.ToString().ToCharArray();
+            return text;
+        }
+        public static char[] ReverseArray(char [] text)
+        {
+            
+            Array.Reverse(text);
+            return text;
+        }
+        public static char [] ReverseArrayManually(char[] text)
         {
             char tmp;
-            int end = arr.Count();
-            for (int i = 0; i < end/2; i++)
+            int end = text.Count();
+            for (int i = 0; i < end / 2; i++)
             {
-                tmp = arr[i];
-                arr[i] = arr[end - i - 1];
-                arr[end - i - 1] = tmp;
+                tmp = text[i];
+                text[i] = text[end - i - 1];
+                text[end - i - 1] = tmp;
 
             }
+            return text;
 
         }
+
         static void Main(string[] args)
         {
-           
 
-            // to do some cool calculations with average time elapsed with long distance
-            // check native methods
-            char[] str = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
-            Console.WriteLine(str);
-            var watch = Stopwatch.StartNew();
-            ReverseWithXOR(ref str);
-            watch.Stop();
-            var elapsedTime = watch.Elapsed;
-            Console.WriteLine(elapsedTime);
+            int[] lengths = new int[] { 1, 10, 25, 50, 100, 1000, 100000 };
 
-            Console.WriteLine(str);
-            watch = Stopwatch.StartNew();
-            ReverseArray(ref str);
-            watch.Stop();
-            elapsedTime = watch.Elapsed;
-            Console.WriteLine(elapsedTime);
-            // second is much faster!
+            Console.WriteLine("Test");
+            char[] str = "Hello world!".ToCharArray();
+            Console.WriteLine(ReverseSB(str)); // value not changed
+            Console.WriteLine(ReverseXOR(str)); // value reversed
+            Console.WriteLine(ReverseArray(str)); // value reversed
+            Console.WriteLine(ReverseArrayManually(str)); // value reversed 
+            foreach (int l in lengths)
+            {
+                int iterations = 10000;
+                char[] text = StringOfLength(l);
+                Benchmark(String.Format("String Builder (Length: {0})", l), ReverseSB, iterations, text); // the slowest on long distances
+                Benchmark(String.Format("Array.Reverse (Length: {0})", l), ReverseArray, iterations, text); // a bit slow on short distances comparing to XOR, but the fastest further
+                Benchmark(String.Format("Xor (Length: {0})", l), ReverseXOR, iterations, text); //  the fastest on short distances
+                Benchmark(String.Format("Reverse Char Array (Length: {0})", l), ReverseArrayManually, iterations, text); //  the second one after array.reverse() in long string cases, but the third one for short strings
 
-            Console.WriteLine(str);
+                Console.WriteLine();
+            }
+
+
+            
+
 
         }
     }
